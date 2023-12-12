1243b3a8b91f28f0c4aedc193c0627047576dca2
tinhtran12345 <tinhtran04012002@gmail.com>
2023-12-12 20:31:26 +0700

    init project

---
+++ README.md
@@ -0,0 +1,54 @@
+## I. Fullstack Newtech_shop project
+
+### 1. Technical Stack
+
+- Backend:
+
+  - [Nest(NestJS)](https://docs.nestjs.com/) is a framework for building efficient, scalable [Node.js](https://nodejs.org/en/download/) server-side application. It uses progressive Javascript, is built with and fully support TypeScript and combines elements of OOP (Object Oriented Programming), FP(Functional Programming), and FRP(Function Reactive Programming)
+
+  - [Prisma ORM](https://www.prisma.io/docs/getting-started/quickstart): is open source next-generation Node.js and TypeScript ORM
+  - [JWT(Json web token)](https://jwt.io/), [Socket.io](socket.io)
+
+- Frontend:
+  - [Nextjs](https://nextjs.org/) using app router, [Redux, Redux-toolkit](https://redux-toolkit.js.org/), [Prisma client](https://www.prisma.io/docs/getting-started/quickstart)
+- Database:
+  - MySQL, [redis](https://redis.io/docs/get-started/)
+
+### 2. Features
+
+- Login, logout, Register, forgot password, read and update personal information, message.
+- Admin features: Manage users, categories, products, posts, promotions, orders, imports, suppliers, statistics.
+- User features: Product details, add or remove products in the cart
+- Order, pay online, view order details, news, purchase history, comments, product reviews
+
+### 3. Build and Deploy
+
+- [Docker](https://www.docker.com/)
+- [Vercel](https://nextjs.org/)
+
+## II. Project details
+
+- Purpose:
+
+### 1. Design Database
+
+- User management
+  - User (full_name, first_name, last_name, email, password)
+  - Address(address_line1, address_line2, city, country, phone_number)
+  - Role (name: admin | user )
+- Product management
+  - Product (SKU,product_name, description, price, quantity)
+  - Product_category(category_name, desc)
+  - Product_discount(name, desc, discount_percent, active) => làm sau
+- Shopping process
+  - Order(order_number, order_date, total_price) --> Order_item(quantity, price)
+  - Shipping_product(order_date, address ,receipt_date)
+  - Payment(date, method, amount, status)
+
+### 2. Preparing data and inserting data to DB
+
+### 3. Project Design
+
+### 4. NestJS
+
+-
---
+++ prisma/migrations/20231212125536_init/migration.sql
@@ -0,0 +1,132 @@
+-- CreateTable
+CREATE TABLE "Address" (
+    "id" SERIAL NOT NULL,
+    "address_line2" TEXT,
+    "city" TEXT,
+    "address_line1" TEXT,
+    "country" TEXT,
+    "phone_number" TEXT,
+    "user_id" INTEGER,
+
+    CONSTRAINT "Address_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Order" (
+    "id" SERIAL NOT NULL,
+    "order_number" INTEGER NOT NULL,
+    "status" TEXT NOT NULL,
+    "total_price" INTEGER NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "note" TEXT,
+    "payment_id" INTEGER NOT NULL,
+
+    CONSTRAINT "Order_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Order_item" (
+    "id" SERIAL NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "price" INTEGER NOT NULL,
+    "order_id" INTEGER NOT NULL,
+    "product_id" INTEGER NOT NULL,
+
+    CONSTRAINT "Order_item_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Payment" (
+    "id" SERIAL NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "payment_method" TEXT NOT NULL,
+    "amount" INTEGER,
+
+    CONSTRAINT "Payment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "SKU" TEXT NOT NULL,
+    "product_name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" INTEGER NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "category_id" INTEGER NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Product_category" (
+    "id" SERIAL NOT NULL,
+    "category_name" TEXT NOT NULL,
+    "desc" TEXT NOT NULL,
+
+    CONSTRAINT "Product_category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Role" (
+    "id" SERIAL NOT NULL,
+    "role_type" TEXT NOT NULL DEFAULT USER,
+    "user_id" INTEGER NOT NULL,
+
+    CONSTRAINT "Role_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Shipping_product" (
+    "id" SERIAL NOT NULL,
+    "shipping_date" DATE DEFAULT CURRENT_TIMESTAMP,
+    "address_line1" TEXT,
+    "address_line2" TEXT,
+    "city" TEXT,
+    "phone_number" TEXT,
+    "receipt_date" DATE,
+    "order_id" INTEGER,
+
+    CONSTRAINT "Shipping_product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Address" ADD CONSTRAINT "Address_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_payment_id_fkey" FOREIGN KEY ("payment_id") REFERENCES "Payment"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Order_item" ADD CONSTRAINT "Order_item_order_id_fkey" FOREIGN KEY ("order_id") REFERENCES "Order"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Order_item" ADD CONSTRAINT "Order_item_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "Product"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Payment" ADD CONSTRAINT "Payment_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "Product_category"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Role" ADD CONSTRAINT "Role_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Shipping_product" ADD CONSTRAINT "Shipping_product_order_id_fkey" FOREIGN KEY ("order_id") REFERENCES "Order"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
