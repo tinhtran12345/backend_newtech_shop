f2aa84a7177f3e155eda6d3841d9145e8d290731
tinhtran12345 <tinhtran04012002@gmail.com>
2023-12-13 01:23:28 +0700

    #1: config docker-compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProviderService } from './provider/provider.service';
+import { ProvidersService } from './providers/providers.service';
+import { ProviderModule } from './provider/provider.module';
+import { ProviderService } from './provider/provider.service';
 
 @Module({
-  imports: [],
+  imports: [ProviderModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ProviderService, ProvidersService],
 })
 export class AppModule {}
