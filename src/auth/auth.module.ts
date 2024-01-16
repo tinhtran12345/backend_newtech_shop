dddb5a3dd192a3e5dc47652ee1bd06733ceacec1
tinhtran <tinhtran04012002@gmail.com>
2024-01-15 23:35:08 -0800

    #5.1. Fix prisma schema

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { PrismaModule } from 'src/providers/prisma.module';
 import { UserModule } from 'src/user/user.module';
 import { JwtService } from '@nestjs/jwt';
 
